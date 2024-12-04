afacc980eff696a11717470d8030faf896d6e2fc
ms-zaman <dev.mszaman@gmail.com>
2024-12-04 08:52:03 +0600

    Projects have been made more structured.

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import Entry from "./components/Entry";
 import Header from "./components/Header";
 
 export default function App() {
-  return (
-    <>
-      <Header />
-      <Entry />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Entry />
+        </>
+    );
 }
